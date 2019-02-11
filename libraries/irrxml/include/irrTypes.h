9f4b98cbb6cfa4c9463cdf8e633a652e9333f69e
nigel <nigel@cresset-group.com>
2019-02-11 17:03:57 +0000

    Fixed Windows 64bit debug build compiler error.

--- libraries/irrxml/include/irrTypes.h
+++ libraries/irrxml/include/irrTypes.h
@@ -80,7 +80,7 @@ typedef unsigned short wchar_t;
 
 //! define a break macro for debugging only in Win32 mode.
 #if defined(WIN32) && defined(_MSC_VER) && defined(_DEBUG)
-#define _IRR_DEBUG_BREAK_IF( _CONDITION_ ) if (_CONDITION_) {_asm int 3}
+#define _IRR_DEBUG_BREAK_IF( _CONDITION_ ) if (_CONDITION_) {__debugbreak();}
 #else 
 #define _IRR_DEBUG_BREAK_IF( _CONDITION_ )
 #endif
